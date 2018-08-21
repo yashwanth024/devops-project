fcced3b05946e146cc5cf91b008449c227543a84
yashwanth024 <39369613+yashwanth024@users.noreply.github.com>
2018-08-21 12:13:44 +0530

    Update SimpleClassTest.java

--- src/test/java/com/vitormp/SimpleClassTest.java
+++ src/test/java/com/vitormp/SimpleClassTest.java
@@ -8,6 +8,6 @@ public class SimpleClassTest extends TestCase {
 
 	@Test
 	public void testname() throws Exception {
-		System.out.println("Hellooooo");
+		System.out.println("Helloooooo0oo0o00");
 	}
 }
